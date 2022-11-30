74846255ae4c6efdeca888cddede8ac55b3cd6f2
Ioannis Antoniou <johnantony24@hotmail.com>
2022-11-30 17:17:42 +0100

    login redirection and topics show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if user_sign_in?
+      country_path(Country.find_by(name: "Switzerland"))
+    else
+      redirect_to home
+    end
+  end
+
+  # def after_sign_out_path_for(resource)
+  #   request.referrer
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  devise_for :users, controllers: {sessions: "sessions"}
 
   resources :user_profiles, only: [:show, :edit, :update] do
     resources :user_tasks, only: [:index, :show, :update]
