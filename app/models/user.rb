07342a6b63452b8c2cdf3dd1c3b655d65d6983c5
marlin coroban <marlin.coroban@gmail.com>
2022-11-30 14:32:25 +0100

    loop through all topics.put topic card inside of a partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :user_profile, dependent: :destroy
-
-  validates :reset_password_token, presence: true
-  validates :reset_password_sent_at, presence: true
-  validates :remember_created_at, presence: true
 end
