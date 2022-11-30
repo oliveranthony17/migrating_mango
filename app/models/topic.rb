db821881cacb4c64322b54c53cc13febb7601028
marlin coroban <marlin.coroban@gmail.com>
2022-11-30 17:32:58 +0100

    added_icons

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,9 @@ class Topic < ApplicationRecord
   belongs_to :country
   validates :title, presence: true
   validates :content, presence: true
+
+
+  def icon
+    read_attribute(:icon)&.html_safe
+  end
 end
