7f34967ceb99b9573f522c626e05a899e604653d
oliveranthony17 <oliver_anthony@outlook.com>
2022-12-06 08:38:55 +0100

    committing to pull master

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -20,50 +20,50 @@ class UserProfilesController < ApplicationController
       case user_profile.entry_method
       when "Work"
         if user_profile.has_job_offer?
-          @user_tasks = Task.where(scope: ["work", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["work", "all"], eu: ["eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["work", "work_find", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["work", "work_find", "all"], eu: ["eu", "both"]).order(order: :asc)
         end
       when "Study"
         if user_profile.has_study_offer?
-          @user_tasks = Task.where(scope: ["study", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["study", "all"], eu: ["eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["study", "study_find", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["study", "study_find", "all"], eu: ["eu", "both"]).order(order: :asc)
         end
       else # family
         if user_profile.has_relative?
-          @user_tasks = Task.where(scope: ["family", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["family", "all"], eu: ["eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["family", "family_find", "all"], eu: ["eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["family", "family_find", "all"], eu: ["eu", "both"]).order(order: :asc)
         end
       end
     else
       case user_profile.entry_method
       when "Work"
         if user_profile.has_job_offer
-          @user_tasks = Task.where(scope: ["work", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["work", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["work", "work_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["work", "work_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         end
       when "Study"
         if user_profile.has_study_offer
-          @user_tasks = Task.where(scope: ["study", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["study", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["study", "study_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["study", "study_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         end
       else # family
         if user_profile.has_relative
-          @user_tasks = Task.where(scope: ["family", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["family", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         else
-          @user_tasks = Task.where(scope: ["family", "family_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
+          @tasks = Task.where(scope: ["family", "family_find", "all"], eu: ["non-eu", "both"]).order(order: :asc)
         end
       end
     end
-    @user_tasks.each do |task|
-      UserTask.create({
+    @tasks.each do |task|
+        UserTask.create({
         task: task,
         user_profile: user_profile,
-        status: "Upcoming"
+        status: task.order == 1 || task.order == 2 ? "Active" : "Upcoming"
       })
     end
     redirect_to user_profile_user_tasks_path(current_user.user_profile)
